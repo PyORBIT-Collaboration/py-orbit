cf0dc240eed4bca4fafc92da78e5aba654d3e878
Andrei Shishlo <shishlo@hotmail.com>
2007-10-11 20:13:03 +0000

    The wrapper around any c++ pointer

---
+++ src/main/pyORBIT_Object.hh
@@ -0,0 +1,35 @@
+//////////////////////////////// -*- C++ -*- //////////////////////////////
+//
+// FILE NAME
+//    pyORBIT_Object.hh
+//
+// AUTHOR
+//    A. Shishlo
+//
+// CREATED
+//    06/08/2007
+//
+// DESCRIPTION
+//    The wrapper around any pointer to the ORBIT c++ class instance.
+//
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef PY_ORBIT_OBJECT_H
+#define PY_ORBIT_OBJECT_H
+
+#include "structmember.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ typedef struct {
+   PyObject_HEAD
+   void* cpp_obj;
+ } pyORBIT_Object;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

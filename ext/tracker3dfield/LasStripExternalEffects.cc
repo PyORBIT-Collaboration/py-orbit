5bebb83c9997a96202f431093667fd253596c2e2
Timofey Gorlov <tvgorlov@gmail.com>
2008-12-15 18:14:53 +0000

    Roll back! - CppBaseFieldSource changed to BaseFieldSource

--- ext/tracker3dfield/LasStripExternalEffects.cc
+++ ext/tracker3dfield/LasStripExternalEffects.cc
@@ -31,6 +31,7 @@
 #include <iomanip>
 #include <cmath>
 #include <fstream>
+#include <complex>
 
 #include "LasStripExternalEffects.hh"
 #include "RungeKuttaTracker.hh"
@@ -131,7 +132,7 @@ void LasStripExternalEffects::applyEffects(Bunch* bunch, int index,
 																			 B_x,B_y,B_z);	
 		
 			
-		fieldSource->getElectricField(xyz[i][0],xyz[i][2],xyz[i][4],t,Ex_stat,Ey_stat,Ez_stat);	
+		fieldSource->getElectricField(xyz[i][0],xyz[i][2],xyz[i][4],t,Ex_stat,Ey_stat,Ez_stat);
 		fieldSource->getMagneticField(xyz[i][0],xyz[i][2],xyz[i][4],t,B_x,B_y,B_z);		
 		LorentzTransformationEM::transform(mass,
 			                                 xyz[i][1],xyz[i][3],xyz[i][5],
--- ext/tracker3dfield/wrap_tracker3dfield.cc
+++ ext/tracker3dfield/wrap_tracker3dfield.cc
@@ -19,7 +19,7 @@ extern "C" {
 		wrap_tracker3dfield::initRungeKuttaTracker(module);
 		wrap_tracker3dfield_py_external_effects::initPyExternalEffects(module);
 		wrap_tracker3dfield_las_strip_external_effects::initLasStripExternalEffects(module);
-		wrap_tracker3dfield_cpp_base_field_source::initBaseFieldSource(module);
+		wrap_tracker3dfield_cpp_base_field_source::initCppBaseFieldSource(module);
 
   }
 	

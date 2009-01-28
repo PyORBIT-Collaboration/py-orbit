f2b8c85a536de8f1e9fd4b5abda2f0af15c68e1f
Andrei Shishlo <shishlo@hotmail.com>
2009-01-28 17:27:12 +0000

    space-charge moved to the core

---
+++ src/spacecharge/Grid2D.cc
@@ -0,0 +1,295 @@
+//This class repersents a 2D rectangular grid 
+
+#include "Grid2D.hh"
+
+#include <iostream>
+
+using namespace OrbitUtils;
+
+// Constructor
+Grid2D::Grid2D(int xSize,    int ySize): CppPyWrapper(NULL)
+{
+	xSize_ = xSize;
+	ySize_ = ySize;
+	xMin_ = -1.0; 
+	xMax_ = +1.0; 
+	yMin_ = -1.0; 
+	yMax_ = +1.0; 
+	init();
+	setZero();
+}
+
+Grid2D::Grid2D( int xSize, int ySize, 
+	             double xMin, double xMax,    
+	             double yMin, double yMax): CppPyWrapper(NULL)
+{
+	xSize_ = xSize;
+	ySize_ = ySize; 
+	xMin_ = xMin;
+	xMax_ = xMax;
+	yMin_ = yMin;
+	yMax_ = yMax;
+	init();
+	setZero();
+}
+
+void Grid2D::init(){
+	
+  if( xSize_ < 3 || ySize_ < 3){
+		int rank = 0;
+		ORBIT_MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+		if(rank == 0){
+			std::cerr << "Grid2D::Grid2D - CONSTRUCTOR \n" 
+         				<< "The grid size too small (should be more than 3)! \n" 
+								<< "number x bins ="<< xSize_ <<" \n"
+								<< "number y bins ="<< ySize_ <<" \n"
+								<< "Stop. \n";
+		}
+		ORBIT_MPI_Finalize();
+  }	
+	
+	dx_ = (xMax_ - xMin_)/(xSize_ -1);
+	dy_ = (yMax_ - yMin_)/(ySize_ -1);
+	arr_ = new double*[xSize_];
+	for(int i = 0; i < xSize_; i++){
+		arr_[i] = new double[ySize_];
+	}
+}
+
+// Destructor
+Grid2D::~Grid2D()
+{
+	//std::cerr<<"debug Grid2D::~Grid2D()"<<std::endl;
+	for(int i = 0; i < xSize_; i++){
+		delete [] arr_[i];
+	}
+	delete [] arr_;
+}
+
+/** Sets the value to the one point of the 2D grid  */	
+void Grid2D::setValue(double value, int ix, int iy){
+	arr_[ix][iy] = value;
+}
+
+/** Returns the interpolated value from the 2D grid */	
+double Grid2D::getValue(double x, double y){
+	int iX, iY;
+	double Wxm, Wx0, Wxp, Wym, Wy0, Wyp;
+	double xFract,  yFract;
+	double xFract2, yFract2;
+	getIndAndFracX(x,iX,xFract);
+	getIndAndFracY(y,iY,yFract);
+	xFract2 = xFract * xFract;
+	yFract2 = yFract * yFract;
+	Wxm = 0.5 * (0.25 - xFract + xFract2);
+	Wx0 = 0.75 - xFract2;
+	Wxp = 0.5 * (0.25 + xFract + xFract2);
+	Wym = 0.5 * (0.25 - yFract + yFract2);
+	Wy0 = 0.75 - yFract2;
+	Wyp = 0.5 * (0.25 + yFract + yFract2);
+	double value =
+	Wxm * Wym * arr_[iX-1][iY-1] +
+	Wxm * Wy0 * arr_[iX-1][iY]   +
+	Wxm * Wyp * arr_[iX-1][iY+1] +
+	Wx0 * Wym * arr_[iX]  [iY-1] +
+	Wx0 * Wy0 * arr_[iX]  [iY]   +
+	Wx0 * Wyp * arr_[iX]  [iY+1] +
+	Wxp * Wym * arr_[iX+1][iY-1] +
+	Wxp * Wy0 * arr_[iX+1][iY]   +
+	Wxp * Wyp * arr_[iX+1][iY+1]; 
+	return value;
+}	
+
+/** Bins the value into the 2D grid */	
+void Grid2D::binValue(double value, double x, double y){
+	int iX, iY;
+	double Wxm, Wx0, Wxp, Wym, Wy0, Wyp;
+	double xFract,  yFract;
+	double xFract2, yFract2;
+	getIndAndFracX(x,iX,xFract);
+	getIndAndFracY(y,iY,yFract);
+	xFract2 = xFract * xFract;
+	yFract2 = yFract * yFract;
+	Wxm = 0.5 * (0.25 - xFract + xFract2);
+	Wx0 = 0.75 - xFract2;
+	Wxp = 0.5 * (0.25 + xFract + xFract2);
+	Wym = 0.5 * (0.25 - yFract + yFract2);
+	Wy0 = 0.75 - yFract2;
+	Wyp = 0.5 * (0.25 + yFract + yFract2);
+	arr_[iX-1][iY-1] += Wxm * Wym * value;      
+  arr_[iX-1][iY]   += Wxm * Wy0 * value;   
+  arr_[iX-1][iY+1] += Wxm * Wyp * value; 
+  arr_[iX]  [iY-1] += Wx0 * Wym * value; 
+  arr_[iX]  [iY]   += Wx0 * Wy0 * value; 
+  arr_[iX]  [iY+1] += Wx0 * Wyp * value; 
+  arr_[iX+1][iY-1] += Wxp * Wym * value; 
+  arr_[iX+1][iY]   += Wxp * Wy0 * value; 
+  arr_[iX+1][iY+1] += Wxp * Wyp * value;             
+}
+
+/** Calculates gradient at a position (x,y) */	
+void Grid2D::calcGradient(double x, double y, double& ex, double& ey){
+	double dWxm,dWx0,dWxp,dWym,dWy0,dWyp;
+	double Wxm, Wx0, Wxp, Wym, Wy0, Wyp;
+	int iX, iY;
+	double xFract,  yFract;
+	double xFract2, yFract2;
+	getIndAndFracX(x,iX,xFract);
+	getIndAndFracY(y,iY,yFract);	
+	xFract2 = xFract * xFract;
+	yFract2 = yFract * yFract;
+	Wxm = 0.5 * (0.25 - xFract + xFract2);
+	Wx0 = 0.75 - xFract2;
+	Wxp = 0.5 * (0.25 + xFract + xFract2);
+	Wym = 0.5 * (0.25 - yFract + yFract2);
+	Wy0 = 0.75 - yFract2;
+	Wyp = 0.5 * (0.25 + yFract + yFract2);	
+  dWxm = (-1.0)*(0.5 - xFract); 
+	dWx0 = (-1.0)*(-2.) * xFract; 
+	dWxp = (-1.0)*(-(0.5 + xFract)); 
+	dWym = (-1.0)*(0.5 - yFract); 
+	dWy0 = (-1.0)*(+2.) * yFract; 
+	dWyp = (-1.0)*(-(0.5 + yFract));
+  ex =
+	dWxm * Wym * arr_[iX-1][iY-1] +
+	dWxm * Wy0 * arr_[iX-1][iY]   +
+	dWxm * Wyp * arr_[iX-1][iY+1] +
+	dWx0 * Wym * arr_[iX]  [iY-1] +
+	dWx0 * Wy0 * arr_[iX]  [iY]   +
+	dWx0 * Wyp * arr_[iX]  [iY+1] +
+	dWxp * Wym * arr_[iX+1][iY-1] +
+	dWxp * Wy0 * arr_[iX+1][iY]   +
+	dWxp * Wyp * arr_[iX+1][iY+1]; 
+	ex = ex / dx_;
+  ey =
+	Wxm * dWym * arr_[iX-1][iY-1] +
+	Wxm * dWy0 * arr_[iX-1][iY]   +
+	Wxm * dWyp * arr_[iX-1][iY+1] +
+	Wx0 * dWym * arr_[iX]  [iY-1] +
+	Wx0 * dWy0 * arr_[iX]  [iY]   +
+	Wx0 * dWyp * arr_[iX]  [iY+1] +
+	Wxp * dWym * arr_[iX+1][iY-1] +
+	Wxp * dWy0 * arr_[iX+1][iY]   +
+	Wxp * dWyp * arr_[iX+1][iY+1];
+	ey = ey / dy_;
+}
+
+/** Calculates gradient at a grid point (ix,iy) */	
+void Grid2D::calcGradient(int iX, int iY, double& ex, double& ey){
+	if(iX != 0 && iX != (xSize_ - 1) && iY != 0 && iY != (ySize_ - 1)){
+		ex =
+		(- 0.125) * arr_[iX-1][iY-1] +
+		(- 0.75 )* arr_[iX-1][iY]   +
+		(- 0.125) * arr_[iX-1][iY+1] +
+		0.125 * arr_[iX+1][iY-1] +
+		0.75  * arr_[iX+1][iY]   +
+		0.125 * arr_[iX+1][iY+1]; 	
+		ex = 0.5 * ex / dx_;
+		ey =
+		(-0.125) * arr_[iX-1][iY-1] +
+		( 0.125) * arr_[iX-1][iY+1] +
+		(-0.75 ) * arr_[iX]  [iY-1] +
+		( 0.75 ) * arr_[iX]  [iY+1] +
+		(-0.125) * arr_[iX+1][iY-1] +
+		( 0.125) * arr_[iX+1][iY+1];
+		ey = 0.5 * ey / dy_;
+		return;
+	} 
+	double x = xMin_ + iX*dx_;
+	double y = yMin_ + iY*dy_;
+	calcGradient(x,y,ex,ey);
+}
+
+/** Sets all grid points to zero */	
+void Grid2D::setZero(){
+	for(int i = 0; i < xSize_; i++){
+		for(int j = 0; j < ySize_; j++){
+			arr_[i][j] = 0.;
+		}
+	}
+}
+
+/** Returns the reference to the 2D array */	
+double** Grid2D::getArr(){
+	return arr_;
+}
+	
+/** Returns the grid size in x-direction */
+int Grid2D::getSizeX(){
+	return xSize_;
+}
+
+/** Returns the grid size in y-direction */
+int Grid2D::getSizeY(){
+	return ySize_;
+}
+
+void Grid2D::getIndAndFracX(double x, int& ind, double& frac){
+   ind  = int ( (x - xMin_)/dx_ + 0.5 );
+   if(ind < 1) ind = 1;
+   if(ind > (xSize_-2)) ind = xSize_ - 2;
+   frac = (x - (xMin_ + ind*dx_))/dx_; 	
+}
+
+void Grid2D::getIndAndFracY(double y, int& ind, double& frac){
+   ind  = int ( (y - yMin_)/dy_ + 0.5 );
+   if(ind < 1) ind = 1;
+   if(ind > (ySize_-2)) ind = ySize_ - 2;
+   frac = (y - (yMin_ + ind*dy_))/dy_; 	
+}	
+
+/** Returns the grid point x-coordinate for this index. */   
+double Grid2D::getGridX(int index){
+	return xMin_ + index*dx_;
+}
+
+/** Returns the grid point y-coordinate for this index. */   
+double Grid2D::getGridY(int index){
+	return yMin_ + index*dy_;
+}
+
+/** Returns the grid step along x-axis */
+double Grid2D::getStepX(){
+	return dx_;
+}
+
+/** Returns the grid step along y-axis */
+double Grid2D::getStepY(){
+	return dy_;
+}
+
+/** Returns the max x in the grid points */ 
+double Grid2D::getMaxX(){return xMax_;};
+
+/** Returns the min x in the grid points */ 
+double Grid2D::getMinX(){return xMin_;};
+
+/** Returns the max y in the grid points */ 
+double Grid2D::getMaxY(){return yMax_;};
+
+/** Returns the min y in the grid points */ 
+double Grid2D::getMinY(){return yMin_;};
+
+/** Sets x-grid */
+void Grid2D::setGridX(double xMin, double xMax){
+	xMax_ = xMin;
+	xMax_ = xMax;
+	dx_ = (xMax_ - xMin_)/(xSize_ -1);
+	setZero();
+}
+
+/** Sets y-grid */
+void Grid2D::setGridY(double yMin, double yMax){
+	yMax_ = yMin;
+	yMax_ = yMax;
+	dy_ = (yMax_ - yMin_)/(ySize_ -1);		
+	setZero();
+}
+
+/** Returns 1 if (x,y) is inside the grid region, and 0 otherwise */
+int Grid2D::isInside(double x,double y){
+	if(x < xMin_ || x > xMax_) return 0;
+	if(y < yMin_ || y > yMax_) return 0;
+	return 1;
+}
+	
---
+++ src/spacecharge/wrap_boundary2d.cc
@@ -0,0 +1,276 @@
+#include "orbit_mpi.hh"
+#include "pyORBIT_Object.hh"
+
+#include "wrap_boundary2d.hh"
+#include "wrap_spacecharge.hh"
+
+#include <iostream>
+
+#include "BaseBoundary2D.hh"
+#include "ShapedBoundary2D.hh"
+#include "Grid2D.hh"
+
+using namespace OrbitUtils;
+
+namespace wrap_spacecharge{
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	//---------------------------------------------------------
+	//Python BaseBoundary2D class definition
+	//---------------------------------------------------------
+
+	//constructor for python class wrapping BaseBoundary2D instance
+	//It never will be called directly
+	static PyObject* Boundary2D_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+	{
+		pyORBIT_Object* self;
+		self = (pyORBIT_Object *) type->tp_alloc(type, 0);
+		self->cpp_obj = NULL;
+		//std::cerr<<"The Boundary2D new has been called!"<<std::endl;
+		return (PyObject *) self;
+	}
+
+  //initializator for python  Boundary2D class
+  //this is implementation of the __init__ method
+  static int Boundary2D_init(pyORBIT_Object *self, PyObject *args, PyObject *kwds){
+		int nPoints, nModes;
+    //if nVars == 2 this is BaseBoundary2D(nPoints, nModes)
+    //if nVars == 4 or 5 this is ShapedBoundary2D(nPoints,nModes,shape,xDim,yDim)
+    int nVars = PyTuple_Size(args);		
+		if(nVars == 2){
+			if(!PyArg_ParseTuple(args,"ii:__init__",&nPoints,&nModes)){
+				ORBIT_MPI_Finalize("PyBoundary2D - Boundary2D(nPoints,nModes) - constructor needs parameters.");
+			}	
+			self->cpp_obj = new BaseBoundary2D(nPoints, nModes);
+			((BaseBoundary2D*) self->cpp_obj)->setPyWrapper((PyObject*) self);
+			//std::cerr<<"The Boundary2D __init__ has been called!"<<std::endl;
+			return 0;
+		}
+		if(nVars == 4 || nVars == 5){
+			const char* shape_name = NULL;
+			double xDim,yDim;
+			if(!PyArg_ParseTuple(args,"iisd|d:__init__",&nPoints,&nModes,&shape_name,&xDim,&yDim)){
+				ORBIT_MPI_Finalize("PyBoundary2D - Boundary2D(nPoints,nModes,shape,xDim,yDim) - constructor needs parameters.");
+			}			
+			string shape(shape_name);	
+			if(nVars == 4){ yDim = xDim;}
+			self->cpp_obj = new ShapedBoundary2D(nPoints, nModes,shape,xDim,yDim);
+			((BaseBoundary2D*) self->cpp_obj)->setPyWrapper((PyObject*) self);
+			//std::cerr<<"The Boundary2D __init__ has been called!"<<std::endl;
+			return 0;			
+		}
+		ORBIT_MPI_Finalize("PyBoundary2D - Boundary2D(nPoints,nModes|shape,xDim,yDim) - constructor needs parameters.");
+		return 0;		
+  }
+	
+	//getMaxX()
+  static PyObject* Boundary2D_getMaxX(PyObject *self, PyObject *args){
+    pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		return Py_BuildValue("d",cpp_Boundary2D->getMaxX());
+	}
+	
+	//getMinX()
+  static PyObject* Boundary2D_getMinX(PyObject *self, PyObject *args){
+    pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		return Py_BuildValue("d",cpp_Boundary2D->getMinX());
+	}
+	//getMaxY()
+  static PyObject* Boundary2D_getMaxY(PyObject *self, PyObject *args){
+    pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		return Py_BuildValue("d",cpp_Boundary2D->getMaxY());
+	}
+	
+	//getMinY()
+  static PyObject* Boundary2D_getMinY(PyObject *self, PyObject *args){
+    pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		return Py_BuildValue("d",cpp_Boundary2D->getMinY());
+	}
+	
+	//getNumberOfPoints()
+	static PyObject* Boundary2D_getNumberOfPoints(PyObject *self, PyObject *args){
+		pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		return Py_BuildValue("i",cpp_Boundary2D->getNumberOfPoints());
+	}
+	
+	//getNumberOfModes()
+	static PyObject* Boundary2D_getNumberOfModes(PyObject *self, PyObject *args){
+		pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		return Py_BuildValue("i",cpp_Boundary2D->getNumberOfModes());
+	}
+	
+	//getShapeName()
+	static PyObject* Boundary2D_getShapeName(PyObject *self, PyObject *args){
+		pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		return Py_BuildValue("s",cpp_Boundary2D->getShapeName().c_str());
+	}
+	
+	
+	//isInitialized()
+	static PyObject* Boundary2D_isInitialized(PyObject *self, PyObject *args){
+		pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		return Py_BuildValue("i",cpp_Boundary2D->isInitialized());
+	}
+	
+	//getBoundaryPoint(index) - returns tuple (x,y)
+	static PyObject* Boundary2D_getBoundaryPoint(PyObject *self, PyObject *args){
+		pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		int index = -1;
+		if(!PyArg_ParseTuple(args,"i:getBoundaryPoint",&index)){
+			ORBIT_MPI_Finalize("PyBoundary2D - getBoundaryPoint(index) - constructor needs a parameter.");
+		}			
+		double x = cpp_Boundary2D->getBoundaryPointX(index);
+		double y = cpp_Boundary2D->getBoundaryPointY(index);
+		return Py_BuildValue("(dd)",x,y);
+	}
+	
+	//setBoundaryPoint(int index, double x, double y)
+	static PyObject* Boundary2D_setBoundaryPoint(PyObject *self, PyObject *args){
+		pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		double x,y;
+		int index = -1;
+		if(!PyArg_ParseTuple(args,"idd:setBoundaryPoint",&index,&x,&y)){
+			ORBIT_MPI_Finalize("PyBoundary2D - setBoundaryPoint(index,x,y) - method needs parameters.");
+		}			
+		cpp_Boundary2D->setBoundaryPoint(index,x,y);
+		Py_INCREF(Py_None);
+    return Py_None;
+	}
+	
+	// initialize() - initializes the all arrays
+	static PyObject* Boundary2D_initialize(PyObject *self, PyObject *args){
+		pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		cpp_Boundary2D->initializeBPs();
+		Py_INCREF(Py_None);
+    return Py_None;
+	}	
+	
+	// addBoundaryPotential(Grid2D* rhoGrid,Grid2D*  phiGrid);
+  static PyObject* Boundary2D_addBoundaryPotential(PyObject *self, PyObject *args){
+    pyORBIT_Object* pyBoundary2D = (pyORBIT_Object*) self;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) pyBoundary2D->cpp_obj;
+		PyObject* pyRhoG;
+		PyObject* pyPhiG;
+		if(!PyArg_ParseTuple(args,"OO:__init__",&pyRhoG,&pyPhiG)){		
+			ORBIT_MPI_Finalize("PyBoundary2D.addBoundaryPotential(rhoGrid2D,phiGrid2D) - method needs parameters.");
+		}	
+		PyObject* pyORBIT_Grid2D_Type = getSpaceChargeType("Grid2D");
+		if(!PyObject_IsInstance(pyRhoG,pyORBIT_Grid2D_Type) || !PyObject_IsInstance(pyPhiG,pyORBIT_Grid2D_Type)){
+			ORBIT_MPI_Finalize("PyBoundary2D.addBoundaryPotential(rhoGrid2D,phiGrid2D) - method needs parameters.");
+		}			
+		Grid2D* grid2D_rho = (Grid2D*)(((pyORBIT_Object*) pyRhoG)->cpp_obj);
+		Grid2D* grid2D_phi = (Grid2D*)(((pyORBIT_Object*) pyPhiG)->cpp_obj);
+		cpp_Boundary2D->addBoundaryPotential(grid2D_rho,grid2D_phi);
+		Py_INCREF(Py_None);
+    return Py_None;	
+	}		
+		
+  //-----------------------------------------------------
+  //destructor for python Boundary2D class (__del__ method).
+  //-----------------------------------------------------
+  static void Boundary2D_del(pyORBIT_Object* self){
+	  //std::cerr<<"The Boundary2D __del__ has been called! 0"<<std::endl;
+		BaseBoundary2D* cpp_Boundary2D = (BaseBoundary2D*) self->cpp_obj;
+		if(cpp_Boundary2D != NULL){
+			delete cpp_Boundary2D;
+		}
+		self->ob_type->tp_free((PyObject*)self);
+		//std::cerr<<"The Boundary2D __del__ has been called! 1"<<std::endl;
+  }
+	
+	// defenition of the methods of the python Boundary2D wrapper class
+	// they will be vailable from python level
+  static PyMethodDef Boundary2DClassMethods[] = {
+		{ "getMaxX",             Boundary2D_getMaxX,             METH_VARARGS,"returns max value in x-direction"},
+		{ "getMinX",             Boundary2D_getMinX,             METH_VARARGS,"returns min value in x-direction"},
+		{ "getMaxY",             Boundary2D_getMaxY,             METH_VARARGS,"returns max value in y-direction"},
+		{ "getMinY",             Boundary2D_getMinY,             METH_VARARGS,"returns min value in y-direction"},
+		{ "getNumberOfModes",    Boundary2D_getNumberOfModes,    METH_VARARGS,"returns the number of free-space modes"},
+		{ "getNumberOfPoints",   Boundary2D_getNumberOfPoints,   METH_VARARGS,"returns the number of boundary points"},
+		{ "getShapeName",        Boundary2D_getShapeName,        METH_VARARGS,"returns the shape name"},
+		{ "isInitialized",       Boundary2D_isInitialized,       METH_VARARGS,"returns 1 if initialized and 0 if not"},
+		{ "initialize",          Boundary2D_initialize,          METH_VARARGS,"initializes all boundary arrays"},
+		{ "getBoundaryPoint",    Boundary2D_getBoundaryPoint,    METH_VARARGS,"returns tuple (x,y) - a boundary point with index"},
+		{ "setBoundaryPoint",    Boundary2D_setBoundaryPoint,    METH_VARARGS,"sets a boindary point (ind,x,y)"},
+		{ "addBoundaryPotential",Boundary2D_addBoundaryPotential,METH_VARARGS,"addBoundaryPotential(Grid2D rhoGrid2D,Grid2D phiGrid2D)"},
+    {NULL}
+  };
+
+	// defenition of the memebers of the python Boundary2D wrapper class
+	// they will be vailable from python level
+	static PyMemberDef Boundary2DClassMembers [] = {
+		{NULL}
+	};
+
+	//new python Boundary2D wrapper type definition
+	static PyTypeObject pyORBIT_Boundary2D_Type = {
+		PyObject_HEAD_INIT(NULL)
+		0, /*ob_size*/
+		"Boundary2D", /*tp_name*/
+		sizeof(pyORBIT_Object), /*tp_basicsize*/
+		0, /*tp_itemsize*/
+		(destructor) Boundary2D_del , /*tp_dealloc*/
+		0, /*tp_print*/
+		0, /*tp_getattr*/
+		0, /*tp_setattr*/
+		0, /*tp_compare*/
+		0, /*tp_repr*/
+		0, /*tp_as_number*/
+		0, /*tp_as_sequence*/
+		0, /*tp_as_mapping*/
+		0, /*tp_hash */
+		0, /*tp_call*/
+		0, /*tp_str*/
+		0, /*tp_getattro*/
+		0, /*tp_setattro*/
+		0, /*tp_as_buffer*/
+		Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
+		"The Boundary2D python wrapper", /* tp_doc */
+		0, /* tp_traverse */
+		0, /* tp_clear */
+		0, /* tp_richcompare */
+		0, /* tp_weaklistoffset */
+		0, /* tp_iter */
+		0, /* tp_iternext */
+		Boundary2DClassMethods, /* tp_methods */
+		Boundary2DClassMembers, /* tp_members */
+		0, /* tp_getset */
+		0, /* tp_base */
+		0, /* tp_dict */
+		0, /* tp_descr_get */
+		0, /* tp_descr_set */
+		0, /* tp_dictoffset */
+		(initproc) Boundary2D_init, /* tp_init */
+		0, /* tp_alloc */
+		Boundary2D_new, /* tp_new */
+	};	
+
+	//--------------------------------------------------
+	//Initialization function of the pyBoundary2D class
+	//It will be called from Bunch wrapper initialization
+	//--------------------------------------------------
+  void initBoundary2D(PyObject* module){
+		if (PyType_Ready(&pyORBIT_Boundary2D_Type) < 0) return;
+		Py_INCREF(&pyORBIT_Boundary2D_Type);
+		PyModule_AddObject(module, "Boundary2D", (PyObject *)&pyORBIT_Boundary2D_Type);
+	}
+
+#ifdef __cplusplus
+}
+#endif
+
+//end of namespace wrap_spacecharge
+}

94277f0272755ea82a72b1172195c2c2502a06f9
Timofey Gorlov <tvgorlov@gmail.com>
2009-03-23 19:06:12 +0000

    Module structure for laser stripping is added

---
+++ py/ext/las_str/__init__.py
@@ -0,0 +1,10 @@
+## \namespace orbit
+## \brief The base module for all pyORBIT core python
+## modules.
+##
+## Some of the modules:
+## - lattice     - the base lattice implementation.
+## - parsers     - collection of parsers.
+## - teapot      - the TEAPOT-like ORBIT lattice elements.
+## - teapot_base - fundamental bricks of teapot elements.
+## - utils       - the collection of useful Python utilites.

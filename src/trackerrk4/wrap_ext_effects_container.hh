124b9b6a6b2c6e724f725e3f85accec7dc6a468d
Timofey Gorlov <tvgorlov@gmail.com>
2009-03-31 00:11:09 +0000

    wraps to container

---
+++ src/trackerrk4/wrap_ext_effects_container.hh
@@ -0,0 +1,21 @@
+#ifndef WRAP_EXT_EFFECTS_CONTAINER_HH_
+#define WRAP_EXT_EFFECTS_CONTAINER_HH_
+
+#include "Python.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  namespace wrap_ext_effects_container{
+    void initExtEffectsContainer(PyObject* module);
+  }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EXT_EFFECTS_CONTAINER_HH_*/
+
+
+

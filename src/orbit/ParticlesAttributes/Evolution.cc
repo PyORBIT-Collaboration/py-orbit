83b5f2d2824751aa5825898086ea2678dd498e2a
Timofey Gorlov <tvgorlov@gmail.com>
2009-11-05 17:30:13 +0000

    attributes modified

--- src/orbit/ParticlesAttributes/Evolution.cc
+++ src/orbit/ParticlesAttributes/Evolution.cc
@@ -16,42 +16,18 @@
 //
 ///////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////
-//
-// INCLUDE FILES
-//
-///////////////////////////////////////////////////////////////////////////
 #include "Bunch.hh"
-
 #include "Evolution.hh"
-///////////////////////////////////////////////////////////////////////////
-//   Constructor and Desctructor
-///////////////////////////////////////////////////////////////////////////
-
-Evolution::Evolution(Bunch* bunch):
-  ParticleAttributes(bunch,0)
-{
-  cl_name_ = "Evolution";
-  attrDescr = "Evol";
-
-}
 
 Evolution::Evolution(Bunch* bunch, int size_in):
   ParticleAttributes(bunch,size_in)
 {
   cl_name_ = "Evolution";
   attrDescr = "Evol";
-	size = size_in;
 }
 
 Evolution::~Evolution()
 {
 }
 
-
-
-//zero element is phase and the last one is the time of i-th particle in the particle frame
-int Evolution::getAttSize(){
-  return size;
-}
 

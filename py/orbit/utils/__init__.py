2ed903b8fa513f018a33fe8a28c8b30b4cb5a8dc
Andrei Shishlo <shishlo@hotmail.com>
2009-11-13 20:22:03 +0000

    The double and int versions are added

--- py/orbit/utils/__init__.py
+++ py/orbit/utils/__init__.py
@@ -2,20 +2,23 @@
 ## \brief Utility classes.
 ##
 ## Classes:
-## - multiDimArray    - Method. Generates multi-dimensional array.
+## - multiDimDoubleArray - Method. Generates multi-dimensional array with doubles.
+## - multiDimIntArray    - Method. Generates multi-dimensional array with integers.
 ## - orbitFinalize    - Method. Finalizes ORBIT script execution.
 ## - NamedObject      - Class. Represents an object with a name.
 ## - TypedObject      - Class. Represents an object with a type.
 ## - ParamsDictObject - Class. Represents an object that has a parameters dictionary.
 
-from orbit.utils.multiDimArray    import multiDimArray
+from orbit.utils.multiDimDoubleArray    import multiDimDoubleArray
+from orbit.utils.multiDimIntArray    import multiDimIntArray
 from orbit.utils.orbitFinalize    import orbitFinalize
 from orbit.utils.NamedObject      import NamedObject
 from orbit.utils.TypedObject      import TypedObject
 from orbit.utils.ParamsDictObject import ParamsDictObject
 
 __all__ = []
-__all__.append("multiDimArray")
+__all__.append("multiDimDoubleArray")
+__all__.append("multiDimIntArray")
 __all__.append("orbitFinalize")
 __all__.append("NamedObject")
 __all__.append("TypedObject")
--- py/orbit/utils/multiDimArray.py
+++ py/orbit/utils/multiDimArray.py
@@ -1,6 +1,25 @@
-def multiDimArray(*dims):
+def multiDimDoubleArray(*dims):
 	"""
-	Method. Creates multi-dimensional arrays, such as a[i][k][j].
+	Method. Creates multi-dimensional arrays with doubles, such as a[i][k][j].
+	Some examples of the use of this function:
+		a = multiDimArray(5,10,2)
+		a = multiDimArray(*[5,10,2])
+		a[1][2][1] = 0.
+		By default all elements are initialized to 0.
+	"""
+	res = []
+	if len(dims) == 1:
+		for j in xrange(dims[0]):
+			res.append(0.)
+	else:
+		dims_rest = dims[1:len(dims)]
+		for j in xrange(dims[0]):
+			res.append(multiDimArray(*dims_rest))
+	return res
+		
+def multiDimIntArray(*dims):
+	"""
+	Method. Creates multi-dimensional arrays with integers, such as a[i][k][j].
 	Some examples of the use of this function:
 		a = multiDimArray(5,10,2)
 		a = multiDimArray(*[5,10,2])
@@ -10,7 +29,7 @@ def multiDimArray(*dims):
 	res = []
 	if len(dims) == 1:
 		for j in xrange(dims[0]):
-			res.append(0.)
+			res.append(0)
 	else:
 		dims_rest = dims[1:len(dims)]
 		for j in xrange(dims[0]):

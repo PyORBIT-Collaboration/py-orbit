32853bbb921b08d71e2d139f25768b7f55ecadf6
Andrei Shishlo <shishlo@hotmail.com>
2010-03-19 21:20:18 +0000

    Next step, but it is not the end.

--- src/utils/OU_Function.cc
+++ src/utils/OU_Function.cc
@@ -21,13 +21,11 @@
 using namespace OrbitUtils;
 
 Function::Function(): CppPyWrapper(NULL)
-{
+{  
+	x_arr = NULL;
+	y_arr = NULL;
+	sizeChunk = 10;
   clean();
-  size = 0;
-  sizeChunk = 50;
-  maxSize = sizeChunk;
-  x_arr = new double[maxSize];
-  y_arr = new double[maxSize];
 
   //MPI stuffs
   rank_MPI = 0;
@@ -50,18 +48,29 @@ Function::~Function()
 
 void Function::resize()
 {
+	int sizeChunk_inner = int( size*0.1);
+	
+	if(sizeChunk_inner < sizeChunk) {
+		sizeChunk_inner = sizeChunk;
+	}
+	
   double* x_tmp = x_arr;
   double* y_tmp = y_arr;
 
-  x_arr = new double[maxSize+sizeChunk];
-  y_arr = new double[maxSize+sizeChunk];
+  maxSize += sizeChunk_inner;
+	
+  x_arr = new double[maxSize];
+  y_arr = new double[maxSize];
 
   for(int i = 0; i < size; i++){
     x_arr[i] = x_tmp[i];
     y_arr[i] = y_tmp[i];
   }
-
-  maxSize += sizeChunk;
+	
+  for(int i = size; i < maxSize; i++){
+    x_arr[i] = 0.;
+    y_arr[i] = 0.;
+  }
 
   delete [] x_tmp;
   delete [] y_tmp;
@@ -89,6 +98,39 @@ void Function::add(double x, double y)
   if(yMin > y) yMin = y;
   if(xMax < x) xMax = x;
   if(yMax < y) yMax = y;
+	if(size > 1){
+		if(x_arr[size-1] < x_arr[size-2]){
+			//the x_arr sorting needed
+			double x_tmp = x_arr[size-1];
+			double y_tmp = y_arr[size-1];	
+			int ind = 0;
+			if(x_tmp > x_arr[0]){
+				int ind_start = 0;
+				int ind_stop = size-2;
+				int count = 0;
+				while((ind_stop - ind_start) > 1){
+					count++;
+					ind = (ind_stop + ind_start)/2;
+					if(x_tmp > x_arr[ind]){
+						ind_start = ind;
+					}
+					else{
+							ind_stop =  ind;
+					}
+					if(count > 200){
+						finalize("ORBIT Utils Function class: The Function method  getX(double y) has unlimited loop. Check data.");
+					}
+				}
+			ind = ind_stop;				
+			}
+			for(int i = size-1; i > ind; i--){
+				x_arr[i] = x_arr[i-1];
+				y_arr[i] = y_arr[i-1];
+			}
+			x_arr[ind] = x_tmp;
+			y_arr[ind] = y_tmp; 			
+		}
+	}
 }
 
 int Function::getSize()
@@ -153,15 +195,23 @@ void Function::clean()
   xMax = -1.0e+300;
   yMin = 1.0e+300;
   yMax = -1.0e+300;
+	
+  if(x_arr != NULL) delete [] x_arr;
+  if(y_arr != NULL) delete [] y_arr;
+	
+  maxSize = sizeChunk;
+	
+  x_arr = new double[maxSize];
+  y_arr = new double[maxSize];
+
+  for(int i = 0; i < maxSize; i++){
+    x_arr[i] = 0.;
+    y_arr[i] = 0.;
+  }	
 }
 
 double Function::getY(double x)
 {
-
-  if(size < 2){
-    finalize("ORBIT Utils Function class:The Function method  getY(double x)  (size<2)");
-  }
-
   if(x <= xMin) return y_arr[0];
   if(x >= xMax) return y_arr[size-1];
 
@@ -173,19 +223,29 @@ double Function::getY(double x)
     if(ind < 0 || ind > (size-2)){
       finalize("ORBIT Utils Function class: The Function method  y(double x)  ind < 0 or ind >= (size-2)");
     }
-    yy = y_arr[ind] + (y_arr[ind+1] - y_arr[ind])*
-      ((x - x_arr[ind])/x_step);
+    yy = y_arr[ind] + (y_arr[ind+1] - y_arr[ind])*((x - x_arr[ind])/x_step);
     return yy;
   }
 
-  while(ind <  (size-1) && x > x_arr[ind]){
-    ind++;
-  }
+  int ind_start = 0;
+  int ind_stop = size-1;
+	int count = 0;
+  while((ind_stop - ind_start) > 1){
+		count++;
+    ind = (ind_stop + ind_start)/2;
+    if(x > x_arr[ind]){
+      ind_start = ind;
+    }
+    else{
+				ind_stop =  ind;
+    }
+		if(count > 200){
+			finalize("ORBIT Utils Function class: The Function method  getX(double y) has unlimited loop. Check data.");
+		}
+  }	
+	ind = ind_start;
 
-  ind--;
-
-  yy = y_arr[ind] + (y_arr[ind+1] - y_arr[ind])*
-    ((x - x_arr[ind])/(x_arr[ind+1] - x_arr[ind]));
+  yy = y_arr[ind] + (y_arr[ind+1] - y_arr[ind])*((x - x_arr[ind])/(x_arr[ind+1] - x_arr[ind]));
   return yy;
 }
 
@@ -214,26 +274,17 @@ double Function::getX(double y)
       ind_start = ind;
     }
     else{
-      if(y == y_arr[ind]){
-				ind_stop = (size-1);
-        ind_start = ind;
-      }
-      else{
 				ind_stop =  ind;
-      }
     }
-		if(count > 10000){
+		if(count > 200){
 			finalize("ORBIT Utils Function class: The Function method  getX(double y) has unlimited loop. Check data.");
 		}
   }
 
-	ind = ind_stop;
-
-  ind--;
+	ind = ind_start;
 
 	if(y_arr[ind+1] != y_arr[ind]){
-    xx = x_arr[ind] + (x_arr[ind+1] - x_arr[ind])*
-    ((y - y_arr[ind])/(y_arr[ind+1] - y_arr[ind]));
+    xx = x_arr[ind] + (x_arr[ind+1] - x_arr[ind])*((y - y_arr[ind])/(y_arr[ind+1] - y_arr[ind]));
 	}
 	else{
 		xx = x_arr[ind];
@@ -253,6 +304,8 @@ int Function::setConstStep(int info)
     inf_const_step = 1;
 
     if(size < 2){
+			inf_const_step = 0;
+			x_step = 0.;
       return 0;
     }
 
@@ -282,6 +335,10 @@ int Function::setInverse(Function* f_inv)
 	
 	if(size < 2){
 		f_inv->clean();
+		if(size == 1){
+			f_inv->add(y(0),x(0));
+			return 1;
+		}
 		return 0;
 	}
 	

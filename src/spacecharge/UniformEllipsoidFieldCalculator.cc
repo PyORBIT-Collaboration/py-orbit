4cedc9475f2097f4f45da53289b376646b315835
Andrei Shishlo <shishlo@hotmail.com>
2010-11-08 22:03:09 +0000

    For the symmetric elliptic integral  the numerical integration was replaced by Carlson algorithm

--- src/spacecharge/UniformEllipsoidFieldCalculator.cc
+++ src/spacecharge/UniformEllipsoidFieldCalculator.cc
@@ -25,18 +25,12 @@ using namespace OrbitUtils;
 /** Constructor. There is no parameters */
 UniformEllipsoidFieldCalculator::UniformEllipsoidFieldCalculator(): CppPyWrapper(NULL)
 {
-
-  uiFunc = new Function();
 	intFuncX = new Function();
 	intFuncY = new Function();
 	intFuncZ = new Function();
-	//sets the default integration point number to 128
-	setIntegralPointsNumber(64);
 	//the number of points
-	lambda_function_points = 200;
-	//the upper limit of integration lambda_max*10^order_of_integration_limit
-	order_of_integration_limit = 3;
-	
+	lambda_function_points = 800;
+
 	//the parameter of ellipses
 	a = 1.; b  = 1.; c = 1.;
 	x_max = 10.; y_max = 10.; z_max = 10.;
@@ -46,7 +40,6 @@ UniformEllipsoidFieldCalculator::UniformEllipsoidFieldCalculator(): CppPyWrapper
 /** Destructor */
 UniformEllipsoidFieldCalculator::~UniformEllipsoidFieldCalculator()
 {
-    delete uiFunc;
 		delete intFuncX;
 		delete intFuncY;
 		delete intFuncZ;
@@ -62,17 +55,7 @@ void UniformEllipsoidFieldCalculator::setEllipsoid(double a_in, double b_in, dou
 	x_max = xmax; y_max = ymax; z_max = zmax;
 	
 	lambda_max = pow(max(max(x_max,y_max),z_max),2);
-	lambda_eps = 0.01*lambda_max/(lambda_function_points - 1);
-	//calculate integrals from lambda_max to lambda_max*10^order_of_integration_limit	
-	double int_valueX0 = 0.;
-	double int_valueY0 = 0.;
-	double int_valueZ0 = 0.;
-	for(int i_order = 0; i_order < order_of_integration_limit; i_order++){
-		int_valueX0 += 1.5*this->integralPhi(a2,b2,c2,lambda_max*pow(10.,i_order),lambda_max*pow(10.,i_order+1));
-		int_valueY0 += 1.5*this->integralPhi(b2,a2,c2,lambda_max*pow(10.,i_order),lambda_max*pow(10.,i_order+1));
-		int_valueZ0 += 1.5*this->integralPhi(c2,b2,a2,lambda_max*pow(10.,i_order),lambda_max*pow(10.,i_order+1));
-	}	
-	
+	lambda_eps = 0.01*lambda_max/(lambda_function_points - 1);	
 	//now integrate the part from lambda to lambda_max for different lambda and put values into functions
 	double lambda_step = lambda_max/(lambda_function_points - 1);
 	double lambda = 0.;	
@@ -82,19 +65,12 @@ void UniformEllipsoidFieldCalculator::setEllipsoid(double a_in, double b_in, dou
 	intFuncX->clean();
 	intFuncY->clean();
 	intFuncZ->clean();
-	for(int iL = 0, nL = (lambda_function_points-1); iL < nL; iL++){
+	for(int iL = 0, nL = lambda_function_points; iL < nL; iL++){
 		lambda = lambda_step*iL;
-		int_valueX = 1.5*this->integralPhi(a2,b2,c2,lambda,lambda_max);
-		int_valueY = 1.5*this->integralPhi(b2,a2,c2,lambda,lambda_max);
-		int_valueZ = 1.5*this->integralPhi(c2,b2,a2,lambda,lambda_max);
-		intFuncX->add(lambda, int_valueX + int_valueX0);
-		intFuncY->add(lambda, int_valueY + int_valueY0);
-		intFuncZ->add(lambda, int_valueZ + int_valueZ0);
+		intFuncX->add(lambda, this->integralPhi(a2,b2,c2,lambda));
+		intFuncY->add(lambda, this->integralPhi(b2,a2,c2,lambda));
+		intFuncZ->add(lambda, this->integralPhi(c2,b2,a2,lambda));
 	}
-	lambda = lambda_step*(lambda_function_points-1);
-	intFuncX->add(lambda, int_valueX0);
-	intFuncY->add(lambda, int_valueY0);
-	intFuncZ->add(lambda, int_valueZ0);	
 	intFuncX->setConstStep(1);
 	intFuncY->setConstStep(1);
 	intFuncZ->setConstStep(1);
@@ -108,22 +84,6 @@ void UniformEllipsoidFieldCalculator::setEllipsoid(double a_in, double b_in, dou
 		}
 		ORBIT_MPI_Finalize();
 	}
-}
-
-/** Calculates integral for int(1.5*(1/(a^2+s))*1/sqrt((a^2+s)*(b^2+s)*(c^2+s)), over s from start_s to stop_s */
-double UniformEllipsoidFieldCalculator::integralPhi(double a_2, double b_2, double c_2, double start_s, double stop_s)
-{
-	double interval = stop_s - start_s;
-	double integral = 0.;
-	double a_s;
-	double s;
-	for(int i = 0, n = uiFunc->getSize(); i < n; i++){
-		s = start_s + interval*uiFunc->x(i);
-		a_s = a_2 +s;
-		integral += uiFunc->y(i)/(a_s*sqrt(a_s*(b_2+s)*(c_2+s)));
-	}
-	integral *= interval;
-	return integral;
 }
 
 /** Calculates the field components */
@@ -165,8 +125,39 @@ void UniformEllipsoidFieldCalculator::calcField(double x,   double y,   double z
 	ez = z*intFuncZ->getY(lambda);
 }
 
-/** Sets the number of integration point in the Gauss-Legendre quadratures */
-void UniformEllipsoidFieldCalculator::setIntegralPointsNumber(int nPoints){	
-	uiFunc->clean();	
-	gauss_legendre_generator(nPoints,0.,1.0,uiFunc);
+/** Calculates integral for int(1.5*(1/(a^2+s))*1/sqrt((a^2+s)*(b^2+s)*(c^2+s)), over s from lambda to infinity */
+double UniformEllipsoidFieldCalculator::integralPhi(double a_2, double b_2, double c_2, double lambda)
+{
+	double x0 = c_2 + lambda;
+	double y0 = b_2 + lambda;
+	double z0 = a_2 + lambda;
+	double x1,y1,z1, mu, nu, X,Y,Z;
+	double sum = 0.;
+	int nIter = 5;
+	for(int i = 0; i < nIter; i++){
+		mu = (x0+y0+3*z0)/5;
+		X = 1.0 - x0/mu;
+		Y = 1.0 - y0/mu;
+		Z = 1.0 - z0/mu;
+		nu = sqrt(x0*y0) + sqrt(z0*y0) + sqrt(z0*x0);
+		x1 = (x0 + nu)*0.25;
+		y1 = (y0 + nu)*0.25;
+		z1 = (z0 + nu)*0.25;
+		sum += 1.0/(pow(4.,i)*(z0+nu)*sqrt(z0));
+		x0 = x1; y0 = y1; z0 = z1;
+	}
+	mu = (x0+y0+3*z0)/5;
+	X = 1.0 - x0/mu;
+	Y = 1.0 - y0/mu;
+	Z = 1.0 - z0/mu;
+	double X2,X3,Y2,Y3,Z2,Z3;
+  X2 = X*X; X3 = X2*X; Y2 = Y*Y; Y3 = Y2*Y; Z2 = Z*Z; Z3 = Z2*Z;
+	double S2 = X2 + Y2 + 3.0*Z2;
+	double S3 = X3 + Y3 + 3.0*Z3;
+	double S4 = X2*X2 + Y2*Y2 + 3.0*Z2*Z2;
+	double S5 = X2*X3 + Y2*Y3 + 3.0*Z2*Z3;
+	sum *= 3;
+	sum += (1.0/(pow(4.,nIter)*sqrt(mu*mu*mu)))*(1.0+3*S2/7+S3/3+3*S2*S2/22+3*S4/11+3*S2*S3/13+3*S5/13);
+	return sum;
 }
+
--- src/spacecharge/UniformEllipsoidFieldCalculator.hh
+++ src/spacecharge/UniformEllipsoidFieldCalculator.hh
@@ -29,27 +29,21 @@ class UniformEllipsoidFieldCalculator: public OrbitUtils::CppPyWrapper
 	
 	/** Sets the half-axis of the ellipsoid and maximal values of x,y,z */
 	void setEllipsoid(double a_in, double b_in, double c_in, double xmax, double ymax, double zmax);
-	
-	/** Sets the number of integration point in the Gauss-Legendre quadratures */
-  void setIntegralPointsNumber(int nPoints);
 
 	/** Calculates the field components */
   void calcField(double x,   double y,   double z, 
 	                 double x2,  double y2,  double z2,
 	                 double& ex, double& ey, double& ez);
   private:
-		
-		/** Calculates integral for int(1.5*(1/(a^2+s))*1/sqrt((a^2+s)*(b^2+s)*(c^2+s)), over s from start_s to stop_s */
-    double integralPhi(double a_2, double b_2, double c_2, double start_s, double stop_s);
 	
+		/** Calculates integral for int(1.5*(1/(a^2+s))*1/sqrt((a^2+s)*(b^2+s)*(c^2+s)), over s from lambda to infinity */
+		static double integralPhi(double a_2, double b_2, double c_2, double lambda);
+		
 	private:
 
 		//the half-axis of the ellipsoid
 		double a,b,c;
 		double a2,b2,c2;
-		
-		//function with gauss_legendre points and weghts for the interval [0,1.0]
-		OrbitUtils::Function* uiFunc; 
 		
 		//maximal values for x,y,z
 		double x_max,y_max,z_max;
@@ -62,7 +56,6 @@ class UniformEllipsoidFieldCalculator: public OrbitUtils::CppPyWrapper
 		
 		//number of points in the function of lambda
 		int lambda_function_points;
-		int order_of_integration_limit;
 		
 		//values of integral for x,y,z axis lambda from 0 to lambda_max = 3*max(x_max^2,y_max^2,z_max^2)
 		// for x int((2/(a^2+s))*1/sqrt((a^2+s)*(b^2+s)*(c^2+s)), over s from lambda to infinity

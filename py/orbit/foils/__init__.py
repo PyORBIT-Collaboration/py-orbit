bacb564e5843e154c5b09b8ac23efcbe936e9b38
Andrei Shishlo <shishlo@hotmail.com>
2012-02-20 19:26:00 +0000

    Foils initial commit

---
+++ py/orbit/foils/__init__.py
@@ -0,0 +1,15 @@
+## \namespace orbit::foil
+## \brief The classes and functions for foils
+##
+## Classes:
+##  - TeapotFoilNode - foil node for the TEAPOT lattices
+## 
+## addTeapotColimatorNode - function to add one collimator node to the lattice
+
+from TeapotFoilNode import TeapotFoilNode
+from foilLatticeModifications import addTeapotFoilNode
+
+__all__ = []
+__all__.append("TeapotFoilNode")
+__all__.append("addTeapotFoilNode")
+

1bd7f6c91aaa5170ff065dc9965e7771da35e7fa
Andrei Shishlo <shishlo@hotmail.com>
2012-11-12 16:43:40 +0000

    Now both functions are parallel.

--- py/orbit/utils/orbit_mpi_utils/bunch_orbit_to_pyorbit.py
+++ py/orbit/utils/orbit_mpi_utils/bunch_orbit_to_pyorbit.py
@@ -1,5 +1,9 @@
 import math
 
+#pyORBIT MPI module import
+import orbit_mpi
+from orbit_mpi import mpi_datatype
+
 from bunch import Bunch
 
 def bunch_orbit_to_pyorbit(ringLength, kineticEnergy, name_of_orbit_mpi_bunch_file, pyOrbitBunch = None):
@@ -12,23 +16,64 @@ def bunch_orbit_to_pyorbit(ringLength, kineticEnergy, name_of_orbit_mpi_bunch_fi
 	pyORBIT: x[m] xp[rad] y[m] yp[rad]  z[m]  dE[GeV]
 	"""
 	L =  ringLength
-	file_in = open(name_of_orbit_mpi_bunch_file,"r")
 	if(pyOrbitBunch == None):  pyOrbitBunch = Bunch()
+	#take the MPI Communicator from bunch: it could be different from MPI_COMM_WORLD
+	comm = pyOrbitBunch.getMPIComm()
+	rank = orbit_mpi.MPI_Comm_rank(comm)
+	size = orbit_mpi.MPI_Comm_size(comm)	
+	main_rank = 0
+	#we will operate with file only at the CPU with rank = 0
+	file_in = None
+	if(rank == main_rank):
+		file_in = open(name_of_orbit_mpi_bunch_file,"r")
+
 	pyOrbitBunch.getSyncParticle().kinEnergy(kineticEnergy)
 
-	ln = file_in.readline()
-	while ln:
-		res_arr = ln.strip().split()
-		val_arr = []
-		for s in res_arr:
-			val_arr.append(float(s))
-		for i in range(4):
-			val_arr[i] /= 1000.
-		val_arr[4] = val_arr[4]*L/(2*math.pi)
-		val_arr[5] = val_arr[5]
-		pyOrbitBunch.addParticle(val_arr[0],val_arr[1],val_arr[2],val_arr[3],val_arr[4],val_arr[5])
-		ln = file_in.readline()
+	# here we will assign ln_nonempty = 1 for each CPU if the line read by CPU with the rank = 0 was non-empty
+	# or  ln_nonempty will be zero everywhere
+	ln = None
+	ln_nonempty = 0
+
+	if(rank == main_rank):
+		ln_nonempty = 0
+		ln = file_in.readline().strip()
+		if(len(ln) > 0):
+			ln_nonempty = 1
+	ln_nonempty = orbit_mpi.MPI_Bcast(ln_nonempty,mpi_datatype.MPI_INT,main_rank,comm)	
+
+	var_arr = (0.,0.,0.,0.,0.,0.)
+	
+	n_count = 1
 		
-	file_in.close()
+	while(ln_nonempty == 1):
+		# the rank of CPU which will get the next particle. It will go through the all CPUs. 
+		recv_rank = n_count % size	
+		if(rank == main_rank):
+			res_arr = ln.strip().split()
+			x  = float(res_arr[0])/1000.
+			xp = float(res_arr[1])/1000.
+			y  = float(res_arr[2])/1000.
+			yp = float(res_arr[3])/1000.
+			z  = float(res_arr[4])*L/(2*math.pi)
+			dE = float(res_arr[5])
+			val_arr = (x,xp,y,yp,z,dE)
+			# send the information if rank = 0 is not going to keep this particle
+			if(recv_rank != main_rank):
+				orbit_mpi.MPI_Send(val_arr,mpi_datatype.MPI_DOUBLE,recv_rank,111,comm)
+			else:
+				pyOrbitBunch.addParticle(val_arr[0],val_arr[1],val_arr[2],val_arr[3],val_arr[4],val_arr[5])
+		if(rank == recv_rank and rank != main_rank):
+			val_arr = orbit_mpi.MPI_Recv(mpi_datatype.MPI_DOUBLE,main_rank,111,comm)
+			pyOrbitBunch.addParticle(val_arr[0],val_arr[1],val_arr[2],val_arr[3],val_arr[4],val_arr[5])
+		# let's again find out if we want to proceed 
+		if(rank == main_rank):
+			ln_nonempty = 0
+			ln = file_in.readline().strip()
+			if(len(ln) > 0):
+				ln_nonempty = 1
+		ln_nonempty = orbit_mpi.MPI_Bcast(ln_nonempty,mpi_datatype.MPI_INT,main_rank,comm)				
+		n_count += 1		
+		
+	if(rank == main_rank): file_in.close()
 	return pyOrbitBunch
 
--- py/orbit/utils/orbit_mpi_utils/bunch_pyorbit_to_orbit.py
+++ py/orbit/utils/orbit_mpi_utils/bunch_pyorbit_to_orbit.py
@@ -2,6 +2,11 @@ import math
 
 from bunch import Bunch
 
+#pyORBIT MPI module import
+import orbit_mpi
+from orbit_mpi import mpi_datatype
+from orbit_mpi import mpi_op
+
 def bunch_pyorbit_to_orbit(ringLength, pyOrbitBunch, name_of_orbit_mpi_bunch_file):
 	"""
 	Translates pyORBIT bunch to ORBIT_MPI bunch and dumps it into the file.
@@ -9,20 +14,71 @@ def bunch_pyorbit_to_orbit(ringLength, pyOrbitBunch, name_of_orbit_mpi_bunch_fil
 	ORBIT_MPI file has lines: x[mm] xp[mrad] y[mm] yp[mrad]   phi[rad]  dE[GeV].
 	pyORBIT: x[m] xp[rad] y[m] yp[rad]  z[m]  dE[GeV]
 	"""	
+	pi2 = 2.0*math.pi
 	L = ringLength
-	file_out = open(name_of_orbit_mpi_bunch_file,"w")
 	b = pyOrbitBunch
-	for i in range(b.getSize()):
-		x = b.x(i)*1000.
-		px = b.px(i)*1000.
-		y = b.y(i)*1000.
-		py = b.py(i)*1000.
-		z = -b.z(i)*2.0*math.pi/L
-		if(z > math.pi):
-			z = z - 2*math.pi
-		if(z < -math.pi):
-			z = z + 2*math.pi
-		dE = b.dE(i)
-		file_out.write(str(x) + " " + str(px) + " " + str(y) + " " + str(py) + " "+ str(z) + " " + str(dE) + "\n")	
-	file_out.close()
+	#take the MPI Communicator from bunch: it could be different from MPI_COMM_WORLD
+	comm = pyOrbitBunch.getMPIComm()
+	rank = orbit_mpi.MPI_Comm_rank(comm)
+	size = orbit_mpi.MPI_Comm_size(comm)	
+	main_rank = 0
+	
+	# n_parts_arr - array of size of the number of CPUs, 
+	# and have the number of macroparticles on each CPU
+	n_parts_arr = []
+	for i_cpu in range(size):
+		if(rank == i_cpu):
+			n_parts_arr.append(b.getSize())
+		else:
+			n_parts_arr.append(0)
+	n_parts_arr = orbit_mpi.MPI_Allreduce(n_parts_arr,mpi_datatype.MPI_INT,mpi_op.MPI_SUM,comm)	
+	
+	file_out = None
+	if(rank == main_rank):
+		file_out = open(name_of_orbit_mpi_bunch_file,"w")
+		
+	if(rank == main_rank):
+		for i in range(n_parts_arr[rank]):
+			x = b.x(i)*1000.
+			px = b.px(i)*1000.
+			y = b.y(i)*1000.
+			py = b.py(i)*1000.
+			z = - (math.fmod(b.z(i)*pi2/L,pi2))
+			if(z > math.pi):
+				z = z - 2*math.pi
+			if(z < -math.pi):
+				z = z + 2*math.pi
+			dE = b.dE(i)
+			file_out.write(str(x) + " " + str(px) + " " + str(y) + " " + str(py) + " "+ str(z) + " " + str(dE) + "\n")				
+	
+	#That is just for case. Actually, MPI_Barrier command is not necessary.
+	orbit_mpi.MPI_Barrier(comm)
+	
+	val_arr = (0.,0.,0.,0.,0.,0.)	
+	
+	for i_cpu in range(1,size):
+		#Again, that is just for case. Actually, MPI_Barrier command is not necessary.
+		orbit_mpi.MPI_Barrier(comm)
+		for i in range(n_parts_arr[i_cpu]):
+			if(rank == main_rank):
+				#get the coordinate array
+				(x,px,y,py,z,dE) = orbit_mpi.MPI_Recv(mpi_datatype.MPI_DOUBLE,i_cpu,222,comm)
+				file_out.write(str(x) + " " + str(px) + " " + str(y) + " " + str(py) + " "+ str(z) + " " + str(dE) + "\n")
+			elif(rank == i_cpu):
+				#send the coordinate array
+				x = b.x(i)*1000.
+				px = b.px(i)*1000.
+				y = b.y(i)*1000.
+				py = b.py(i)*1000.
+				z = - (math.fmod(b.z(i)*pi2/L,pi2))
+				if(z > math.pi):
+					z = z - 2*math.pi
+				if(z < -math.pi):
+					z = z + 2*math.pi
+				dE = b.dE(i)
+				val_arr = (x,px,y,py,z,dE)
+				orbit_mpi.MPI_Send(val_arr,mpi_datatype.MPI_DOUBLE,main_rank,222,comm)
+			
+	if(rank == main_rank):				
+		file_out.close()
 

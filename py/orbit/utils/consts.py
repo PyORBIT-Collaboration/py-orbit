6364a98e64e0e682f8c229e4723e00631adffee2
Sarah Cousineau <scousine@ornl.gov>
2013-05-14 18:28:09 +0000

    new consts dictionary

---
+++ py/orbit/utils/consts.py
@@ -0,0 +1,7 @@
+
+speed_of_light = 2.99792e8 # m/s
+pi = 3.14159265
+mass_proton =  0.938272029 # GeV/c^2
+mass_electron = 0.000511 # GeV/c^2
+mass_electron_mks =  9.10938291e-31 # kg
+charge_electron = 1.602176e-19 # Coulomb

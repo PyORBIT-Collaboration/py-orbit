22131467a7b0bf259ad5b9d5d409af773cb44372
Sarah Cousineau <scousine@ornl.gov>
2013-05-17 19:09:13 +0000

    python field tracker

---
+++ py/orbit/fieldtracker/__init__.py
@@ -0,0 +1,14 @@
+## 
+## \brief The classes for fieldtracker according to different models.
+##
+## Classes:
+
+
+from fieldtracker import FieldTracker
+from parsefield import FieldParser3D
+
+__all__ = []
+__all__.append("FieldTracker")
+__all__.append("trackBunch")
+__all__.append("FieldParser3D")
+

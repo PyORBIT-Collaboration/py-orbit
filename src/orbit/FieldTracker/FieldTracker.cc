5b83c270d2c2c7fddff3e82401d3c40d345ec6aa
reatmj <reatmj@gmail.com>
2013-05-17 13:57:05 +0000

    new package

---
+++ src/orbit/FieldTracker/FieldTracker.cc
@@ -0,0 +1,36 @@
+#include "FieldTracker.hh"
+
+
+
+#include <iostream>
+#include <cmath>
+#include <cfloat>
+#include <cstdlib>
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+// Comments and Stuff
+//
+//
+//
+//
+//////////////////////////////////////////////////////////////////////////
+
+//Constructor
+
+FieldTracker::FieldTracker()
+{
+    
+    
+    
+}
+
+FieldTracker:TrackBunch(Bunch b)
+{
+    
+    
+    
+    
+    
+}

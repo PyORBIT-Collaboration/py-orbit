8a1e63681841da304cedd78a596f64615ac326f3
Sarah Cousineau <scousine@ornl.gov>
2013-05-19 12:25:42 +0000

    new wrappers

---
+++ src/orbit/FieldTracker/wrap_fieldtracker.hh
@@ -0,0 +1,19 @@
+#ifndef WRAP_ORBIT_FIELDTRACKER_HH_
+#define WRAP_ORBIT_FIELDTRACKER_HH_
+
+#include "Python.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    namespace wrap_fieldtracker{
+        void initfieldtracker();
+    }
+    
+#ifdef __cplusplus
+}
+
+#endif
+
+#endif /*WRAP_ORBIT_FIELDTRACKER_HH_*/

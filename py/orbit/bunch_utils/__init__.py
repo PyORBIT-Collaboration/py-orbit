35846e701cf9bfeb6844c1dbfd62f633f8ba59a1
Sarah Cousineau <scousine@ornl.gov>
2014-03-11 15:48:32 +0000

    Deleted accidentally copied lines.

--- py/orbit/bunch_utils/__init__.py
+++ py/orbit/bunch_utils/__init__.py
@@ -9,11 +9,5 @@ from particleidnumber import ParticleIdNumber
 
 __all__ = []
 __all__.append("addParticleIdNumbers")
-__all__.append("TeapotInjectionNode")
-__all__.append("InjectParts")
-__all__.append("JohoTransverse")
-__all__.append("JohoLongitudinal")
-__all__.append("UniformLongDist")
-__all__.append("SNSESpreadDist")
-__all__.append("GULongDist")
+
 

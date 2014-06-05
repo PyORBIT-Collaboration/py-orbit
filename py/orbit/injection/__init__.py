208b47fadb087b92f7213905c61ca006baea97c7
cericks7 <cericks7.utk@gmail.com>
2014-06-05 16:26:32 +0000

    New pulse width painting class

--- py/orbit/injection/__init__.py
+++ py/orbit/injection/__init__.py
@@ -10,7 +10,7 @@ from injectparticles import InjectParts
 from joho import JohoTransverse, JohoLongitudinal
 from InjectionLatticeModifications import addTeapotInjectionNode
 from TeapotInjectionNode import TeapotInjectionNode
-from distributions import UniformLongDist, GULongDist, SNSESpreadDist
+from distributions import UniformLongDist, UniformLongDistPaint, GULongDist, SNSESpreadDist, SNSESpreadDistPaint
 
 __all__ = []
 __all__.append("addTeapotInjectionNode")
@@ -19,6 +19,8 @@ __all__.append("InjectParts")
 __all__.append("JohoTransverse")
 __all__.append("JohoLongitudinal")
 __all__.append("UniformLongDist")
+__all__.append("UniformLongDistPaint")
 __all__.append("SNSESpreadDist")
+__all__.append("SNSESpreadDistPaint")
 __all__.append("GULongDist")
 

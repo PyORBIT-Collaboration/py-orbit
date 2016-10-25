711db76e0269a3e8fc3dbb07528a4eeae15436af
Andrei Shishlo <shishlo@hotmail.com>
2016-10-25 19:16:02 +0000

    The functions for aperture adding to the linac lattices have been added for bend and drift nodes.

--- py/orbit/py_linac/lattice_modifications/__init__.py
+++ py/orbit/py_linac/lattice_modifications/__init__.py
@@ -3,14 +3,18 @@
 ##
 
 from apertures_additions_lib import Add_quad_apertures_to_lattice
+from apertures_additions_lib import Add_bend_apertures_to_lattice
 from apertures_additions_lib import Add_rfgap_apertures_to_lattice
 from apertures_additions_lib import GetLostDistributionArr
 from apertures_additions_lib import AddScrapersAperturesToLattice
+from apertures_additions_lib import Add_drift_apertures_to_lattice
 from sns_aperture_additions import AddMEBTChopperPlatesAperturesToSNS_Lattice
 
 __all__ = []
 __all__.append("Add_quad_apertures_to_lattice")
+__all__.append("Add_bend_apertures_to_lattice")
 __all__.append("Add_rfgap_apertures_to_lattice")
 __all__.append("GetLostDistributionArr")
 __all__.append("AddScrapersAperturesToLattice")
+__all__.append("Add_drift_apertures_to_lattice")
 __all__.append("AddMEBTChopperPlatesAperturesToSNS_Lattice")
--- py/orbit/py_linac/lattice_modifications/apertures_additions_lib.py
+++ py/orbit/py_linac/lattice_modifications/apertures_additions_lib.py
@@ -10,10 +10,10 @@ import sys
 import os
 
 from orbit.py_linac.lattice import LinacApertureNode
-from orbit.py_linac.lattice import Quad, AbstractRF_Gap
+from orbit.py_linac.lattice import Quad, Drift, Bend, AbstractRF_Gap
 from orbit.py_linac.overlapping_fields import OverlappingQuadsNode
 
-def Add_quad_apertures_to_lattice(accLattice,aprtNodes=[]):
+def Add_quad_apertures_to_lattice(accLattice, aprtNodes=[]):
 	"""
 	Function will add Aperture nodes at the entrance and exit of quads.
 	It returns the list of Aperture nodes.
@@ -73,7 +73,47 @@ def Add_quad_apertures_to_lattice(accLattice,aprtNodes=[]):
 	return aprtNodes
 	
 
-def Add_rfgap_apertures_to_lattice(accLattice,aprtNodes=[]):
+def Add_bend_apertures_to_lattice(accLattice, aprtNodes=[], step = 0.25):
+	"""
+	Function will add Aperture nodes at the bend nodes of the lattice if they
+	have aperture parameters.
+	The distance between aperture nodes is defined by the step input parameter 
+	(in meters).
+	"""
+	node_pos_dict = accLattice.getNodePositionsDict()
+	bends = accLattice.getNodesOfClasses([Bend,])
+	if(len(bends) <= 0): return aprtNodes
+	for node in bends:
+		if(node.hasParam("aperture_x") and node.hasParam("aperture_y") and node.hasParam("aprt_type")): 
+			shape = node.getParam("aprt_type")
+			a_x = node.getParam("aperture_x")			
+			a_y = node.getParam("aperture_y")
+			if(shape != 3): continue
+			node_name = node.getName()
+			(posBefore, posAfter) = node_pos_dict[node]
+			nParts = node.getnParts()
+			s = posBefore
+			pos_part_arr = []
+			for part_ind in range(nParts):
+				pos_part_arr.append(s)
+				s += node.getLength(part_ind)
+			run_path = pos_part_arr[0]
+			run_path_old = pos_part_arr[0] - 2*step
+			for part_ind in range(nParts):
+				run_path = pos_part_arr[part_ind]
+				if(run_path >= run_path_old + step):	
+					apertureNode = LinacApertureNode(shape,a_x/2.0,a_y/2.0,run_path)
+					aprt_name = node_name+":"+str(part_ind)+":Aprt"
+					if(nParts == 1): aprt_name = node_name+":Aprt"					
+					apertureNode.setName(aprt_name)
+					apertureNode.setSequence(node.getSequence())
+					node.addChildNode(apertureNode,node.BODY,part_ind,node.BEFORE)		
+					aprtNodes.append(apertureNode)
+					run_path_old = run_path
+	return aprtNodes			
+
+
+def Add_rfgap_apertures_to_lattice(accLattice, aprtNodes=[]):
 	"""
 	Function will add Aperture nodes at the entrance and exit of RF gap.
 	It returns the list of Aperture nodes.
@@ -99,7 +139,66 @@ def Add_rfgap_apertures_to_lattice(accLattice,aprtNodes=[]):
 	return aprtNodes
 
 
-def GetLostDistributionArr(aprtNodes,bunch_lost):
+def Add_drift_apertures_to_lattice(accLattice, pos_start, pos_end, step, aperture_d, aprtNodes=[]):
+	"""
+	Function will add Aperture nodes at the drift nodes of the lattice between
+	positions pos_start and pos_end with the minimal distance of 'step' between 
+	aperture nodes. The shape of this apertures defines here is always shape=1
+	which means a circle with the diameter = aperture_d in meters.
+	It returns the list of Aperture nodes.
+	"""
+	shape = 1
+	a = aperture_d
+	node_pos_dict = accLattice.getNodePositionsDict()
+	drifts = accLattice.getNodesOfClasses([Drift,])
+	if(len(drifts) <= 0): return aprtNodes
+	#---- run_path = posBefore in (posBefore, posAfter) = node_pos_dict[node]
+	run_path = node_pos_dict[drifts[0]][0]
+	run_path_old = run_path - 2*step	
+	for node in drifts:	
+		node_name = node.getName()
+		(posBefore, posAfter) = node_pos_dict[node]
+		if(posBefore > pos_end): break
+		nParts = node.getnParts()
+		s = posBefore
+		pos_part_arr = []
+		for part_ind in range(nParts):
+			pos_part_arr.append(s)
+			s += node.getLength(part_ind)
+		for part_ind in range(nParts):
+			run_path = pos_part_arr[part_ind]
+			if(run_path >= pos_start and run_path <= pos_end):
+				if(run_path >= run_path_old + step):
+					apertureNode = LinacApertureNode(shape,a/2.0,a/2.0,run_path)
+					aprt_name = node_name+":"+str(part_ind)+":Aprt"
+					if(nParts == 1): aprt_name = node_name+":Aprt"
+					apertureNode.setName(aprt_name)
+					apertureNode.setSequence(node.getSequence())
+					node.addChildNode(apertureNode,node.BODY,part_ind,node.BEFORE)		
+					aprtNodes.append(apertureNode)
+					run_path_old = run_path
+	return aprtNodes				
+				
+
+def AddScrapersAperturesToLattice(accLattice, node_name, x_size, y_size, aprtNodes=[]):
+	"""
+	Function will add the rectangular Aperture node (shape=3) at the node with a particular name.
+	Parameters x_size and y_size are full horizontal and vertical sizes of the aperture.
+	"""
+	shape = 3
+	node_pos_dict = accLattice.getNodePositionsDict()
+	node = accLattice.getNodesForName(node_name)[0]
+	(posBefore, posAfter) = node_pos_dict[node]
+	apertureNode = LinacApertureNode(shape,x_size/2.0,y_size/2.0,posBefore)
+	apertureNode.setName(node_name+":Aprt")
+	apertureNode.setSequence(node.getSequence())
+	node.addChildNode(apertureNode,node.ENTRANCE)
+	aprtNodes.append(apertureNode)
+	aprtNodes = sorted(aprtNodes, key = lambda x: x.getPosition(), reverse = False)
+	return aprtNodes	
+	
+
+def GetLostDistributionArr(aprtNodes, bunch_lost):
 	"""
 	Function returns the array with [aptrNode,sum_of_losses]
 	The sum_of_losses is a number of particles or the sum of macrosizes if the 
@@ -146,24 +245,6 @@ def GetLostDistributionArr(aprtNodes,bunch_lost):
 			continue
 		lossDist_arr[pos_ind][1] += 1.0
 	return lossDist_arr
-			
-
-def AddScrapersAperturesToLattice(accLattice,node_name,x_size,y_size,aprtNodes=[]):
-	"""
-	Function will add the rectangular Aperture node (shape=3) at the node with a particular name.
-	Parameters x_size and y_size are full horizontal and vertical sizes of the aperture.
-	"""
-	shape = 3
-	node_pos_dict = accLattice.getNodePositionsDict()
-	node = accLattice.getNodesForName(node_name)[0]
-	(posBefore, posAfter) = node_pos_dict[node]
-	apertureNode = LinacApertureNode(shape,x_size/2.0,y_size/2.0,posBefore)
-	apertureNode.setName(node_name+":Aprt")
-	apertureNode.setSequence(node.getSequence())
-	node.addChildNode(apertureNode,node.ENTRANCE)
-	aprtNodes.append(apertureNode)
-	aprtNodes = sorted(aprtNodes, key = lambda x: x.getPosition(), reverse = False)
-	return aprtNodes			
 		
 			
 		

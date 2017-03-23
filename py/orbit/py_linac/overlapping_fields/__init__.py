b13940461d2ed6566be032ca6dcd117138a1a73f
shishlo <shishlo@hotmail.com>
2017-03-23 11:39:09 +0900

    The J-PARC Enge (or in general - distributed quad field) function factory was added.

--- py/orbit/py_linac/overlapping_fields/__init__.py
+++ py/orbit/py_linac/overlapping_fields/__init__.py
@@ -5,11 +5,15 @@
 from overlapping_quad_fields_lib import EngeFunction
 from overlapping_quad_fields_lib import AbstractQuadFieldSourceFunction
 from overlapping_quad_fields_lib import SimpleQuadFieldFunc
+from overlapping_quad_fields_lib import PMQ_Trace3D_Function
 
 from sns_enge_func_factory import SNS_EngeFunctionFactory
+from jparc_enge_func_factory import JPARC_EngeFunctionFactory
 
 __all__ = []    
 __all__.append("EngeFunction")
 __all__.append("AbstractQuadFieldSourceFunction")
 __all__.append("SimpleQuadFieldFunc")
+__all__.append("PMQ_Trace3D_Function")
 __all__.append("SNS_EngeFunctionFactory")
+__all__.append("JPARC_EngeFunctionFactory")

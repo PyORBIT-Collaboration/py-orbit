2fd3ed4f6291cd3fa5a2f2ea775edd885982fe75
sappel <s.appel@gsi.de>
2017-08-16 08:42:46 +0200

    Update __init__.py
    remove import of beamline

--- py/orbit/matrix_lattice/__init__.py
+++ py/orbit/matrix_lattice/__init__.py
@@ -4,10 +4,8 @@
 ## These classes use orbit::utils::matrix::Matrix C++ wrappers
 from MATRIX_Lattice import MATRIX_Lattice
 from BaseMATRIX import BaseMATRIX
-from PlotBeamline import PlotBeamline
 
 __all__ = []
 __all__.append("MATRIX_Lattice")
 __all__.append("BaseMATRIX")
-__all__.append("PlotBeamline")
 

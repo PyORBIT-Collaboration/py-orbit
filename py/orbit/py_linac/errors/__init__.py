a0fcd71176d4ecb907f0694ceeb76d06764b244b
shishlo <shishlo@hotmail.com>
2021-05-19 18:21:39 -0400

    Initial commit for errors + linacs and trasport lines orbit correction

--- py/orbit/py_linac/errors/__init__.py
+++ py/orbit/py_linac/errors/__init__.py
@@ -10,11 +10,22 @@ from ErrorNodesAndControllersLib import ErrorCntrlCoordDisplacement
 
 __all__ = []
 
-#---- Error Controllers classes
-__all__.append("BaseErrorController")
-__all__.append("ErrorCntrlCoordDisplacement")
-
 
 #---- Error nodes classes
 __all__.append("AccErrorNode")
+__all__.append("ErrorLongitudinalDisplacementNode")
 __all__.append("ErrorCoordDisplacementNode")
+__all__.append("ErrorStraightRotationZNode")
+__all__.append("ErrorStraightRotationXNode")
+__all__.append("ErrorStraightRotationYNode")
+
+#---- Error Controllers classes
+__all__.append("BaseErrorController")
+__all__.append("ErrorCntrlLongitudinalDisplacement")
+__all__.append("ErrorCntrlCoordDisplacement")
+__all__.append("ErrorCntrlStraightRotationZ")
+__all__.append("ErrorCntrlStraightRotationX")
+__all__.append("ErrorCntrlStraightRotationY")
+
+
+
---
+++ py/orbit/py_linac/orbit_correction/__init__.py
@@ -0,0 +1,10 @@
+## \namespace orbit::py_linac::orbit_correction
+## \Classes and packages of ORBIT Linac.
+##
+
+from trasport_lines_orbit_correction import BaseOrbitCorrection
+
+__all__ = []
+
+#---- Transport lines orbit correction classes
+#__all__.append("BaseOrbitCorrection")
